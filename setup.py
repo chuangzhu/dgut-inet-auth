503f84b5dfb5d10aaf2b1ed37f150afd3b82cd81
Chuang Zhu <chuang@melty.land>
2021-11-15 22:03:10 +0800

    + setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='dgut-inet-auth',
+      version='0.1',
+      author='Zhu Chuang',
+      author_email='chuang at melty dot land',
+      packages=[],
+      scripts=['dgut-inet-auth'],
+      python_requires='>=3.7',
+      install_requires=['requests', 'pycryptodome'])
